a92eda395bfbe177c499a9d234d075bddbf4be17
explorerwjy <explorerwjy@gmail.com>
2016-03-18 04:59:53 +0000

    task 7 ok

--- PS04/wikipedia_by_month.py
+++ PS04/wikipedia_by_month.py
@@ -14,8 +14,8 @@ import sys
 from operator import add
 from pyspark import SparkContext
 
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
+#import matplotlib.mlab as mlab
+#import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     
@@ -31,27 +31,30 @@ if __name__ == "__main__":
 
     sc     = SparkContext( appName="Wikipedia Count" )
     ## YOUR CODE GOES HERE
-	lines = sc.textFile(infile)
-	datetime = lines.map(lambda x:x.split('\t')[2])
-	date = datetime.map(lambda x:'-'.join(x.split('-')[0:2]))
-	pairs = date(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)
-	counts = pairs.sortByKey()
+    lines = sc.textFile(infile)
+    datetime = lines.map(lambda x:x.split('\t')[2])
+    date = datetime.map(lambda x:'-'.join(x.split('-')[0:2]))
+    pairs1 = date.map(lambda x:(x,1))
+    pairs2 = pairs1.reduceByKey(lambda x,y:x+y)
+    counts = pairs2.sortByKey(False).collect()
     ## PUT YOUR RESULTS IN counts
 
-	x = []
-	y = []
+    #x = []
+    #y = []
     with open("wikipedia_by_month.txt","w") as fout:
         for (date, count) in counts:
             fout.write("{}\t{}\n".format(date,count))
-			x.append(date)
-			y.append(count)
+	    #x.append(date)
+	    #y.append(count)
 	##plot
-	plt.plot(x,y,'r--',linewidth=1)
-	plt.xlabel('Date')
-	plt.ylabel('Count')
-	plt.title('wikipedia monthly totals')
-	plt.grid(True)
-	plt.show()
+    
+    #plt.plot(x,y,'r--',linewidth=1)
+    #plt.xlabel('Date')
+    #plt.ylabel('Count')
+    #plt.title('wikipedia monthly totals')
+    #plt.grid(True)
+    #plt.show()
+   
     ## 
     ## Terminate the Spark job
     ##
